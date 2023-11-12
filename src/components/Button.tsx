6e97c042b3b6c5dd2b64a4cb29397266007d7b39
ing-arriola <ingeniero.arriola@gmail.com>
2023-11-12 17:54:42 -0600

    added first version of button

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <div  className='flex p-5 justify-center rounded-full opacity-75 text-lg bg-primaryColor font-thin italic w-56'>Confirma tu asistencia</div>
+  )
+}
