b57b304f9d7e6a177756f0969019aecfaf809c8f
ing-arriola <ingeniero.arriola@gmail.com>
2023-11-13 06:21:53 -0600

    updated responsive design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Button = () => {
   return (
-    <div  className='flex p-5 justify-center rounded-full opacity-75 text-lg bg-primaryColor font-thin italic w-56'>Confirma tu asistencia</div>
+    <div  className='flex p-3 md:p-5 justify-center rounded-full opacity-75 text-base md:text-lg bg-primaryColor font-thin italic w-52 md:w-56'>Confirma tu asistencia</div>
   )
 }
