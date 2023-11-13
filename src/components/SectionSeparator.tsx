0d1b8ae18a85a8645f51e316d424b01a388c345b
ing-arriola <ingeniero.arriola@gmail.com>
2023-11-13 12:42:20 -0600

    added parameter

--- src/components/SectionSeparator.tsx
+++ src/components/SectionSeparator.tsx
@@ -1,10 +1,11 @@
 interface sectionSeparatorProps{
   topMargin?:boolean
+  bottomMargin?:boolean
 }
 
-export const SectionSeparator = ({topMargin=true}:sectionSeparatorProps) => {
+export const SectionSeparator = ({topMargin=true, bottomMargin=true}:sectionSeparatorProps) => {
   return (
-    <section className={`flex justify-center ${topMargin?'mt-10':''} mb-10`}>
+    <section className={`flex justify-center ${topMargin?'mt-10':''} ${bottomMargin?'mb-10':''} `}>
       <img src='/sectionSeparator.svg' />
      </section>
   )
