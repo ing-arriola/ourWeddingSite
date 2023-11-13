fd283bc5ef881669aa3f6494fffda52c85e0a6a0
ing-arriola <ingeniero.arriola@gmail.com>
2023-11-13 12:59:28 -0600

    added parameter

--- src/components/PrimaryText.tsx
+++ src/components/PrimaryText.tsx
@@ -1,10 +1,11 @@
 interface primaryTextProps{
     text:string;
-    thin?:boolean
+    thin?:boolean;
+    styles?:string;
 }
 
-export const PrimaryText = ({text,thin=false}:primaryTextProps) => {
+export const  PrimaryText = ({text,thin=false,styles}:primaryTextProps) => {
   return (
-    <label className={`text-primaryColor text-lg ${thin?'font-thin':''}`}>{text}</label>
+    <label className={`text-primaryColor text-lg ${thin?'font-thin':''} ${styles}`}>{text}</label>
   )
 }
