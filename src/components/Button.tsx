ee7c18ebbc25735ec7fded9f7b7339d237a3e541
ing-arriola <ingeniero.arriola@gmail.com>
2023-11-13 23:24:43 -0600

    added props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+interface ButtonProps {
+  onClick:()=>void;
+  label:string
+}
 
-export const Button = () => {
+export const Button = ({onClick,label}:ButtonProps) => {
   return (
-    <div  className='flex p-3 md:p-5 justify-center rounded-full opacity-75 text-base md:text-lg bg-primaryColor font-thin italic w-52 md:w-56'>Confirma tu asistencia</div>
+    <button onClick={onClick} className='flex p-3 md:p-3 justify-center rounded-full opacity-75 text-base md:text-lg bg-primaryColor font-thin italic w-52 md:w-56  transition-transform duration-300 transform hover:translate-y-[-0.3rem] hover:shadow-md active:translate-y-[-0.1rem] active:shadow-sm relative'>
+      {label}
+    </button>
   )
 }
